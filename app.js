d0e07f919cd868fd470f7d6bf4eea947c4fd8d65
seanagibson <seanandgibson@gmail.com>
2015-08-12 16:49:14 -0700

    added airports route

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var flightSearch = require('./flightSearch');
+var FlightSearch = require('./flightSearch');
+var AirportData = require('./airportdata');
 var app = express();
+var flightSearch = new FlightSearch();
+var airportData = new AirportData();
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/views'));
@@ -9,16 +12,22 @@ app.get('/', function(req, res){
   res.sendFile('index.html');
 });
 
+app.get('/airports', function(req, res){
+  var airportArray = [];
+  airportArray = airportData.getAirports();
+  res.json(airportArray);
+});
+
 app.get('/search', function(req, res){
   var flightResults = {};
   var depart = req.query.departId;
   var destination = req.query.destId;
 
-  flightResults = flightSearch(depart, destination);
+  flightResults = flightSearch.searchResults(depart, destination);
 
   if(flightResults.itinerary.length === 0){
     res.status(500);
-    res.render('error', 'Error: Could not locate airports');
+    res.render('error', 'Internal Error');
   } else {
     res.json(flightResults);
   }
